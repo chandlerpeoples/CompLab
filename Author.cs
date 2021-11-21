6954ce17e59b43a0e918256912df6f8c7f8ebf85
chandlerpeoples <cbpeoples1@crimson.ua.edu>
2021-11-21 15:14:51 -0600

    finished

---
+++ Author.cs
@@ -0,0 +1,49 @@
+namespace CompLab
+{
+    public class Author
+    {
+        private string name;
+        private char gender;
+        private string email;
+
+        public Author()
+        {
+
+        }
+        public Author(string name, char gender, string email)
+        {
+            this.name = name;
+            this.gender = gender;
+            this.email = email;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+        public char GetGender()
+        {
+            return gender;
+        }
+        public void SetGender(char gender)
+        {
+            this.gender = gender;
+        }
+        public string GetEmail()
+        {
+            return name;
+        }
+        public void SetEmail(string email)
+        {
+            this.email = email;
+        }
+        public override string ToString()
+        {
+            return "Name: " + name + " Email: " + email + " Gender: " + gender;
+        }
+    }
+}
---
+++ Book.cs
@@ -0,0 +1,48 @@
+namespace CompLab
+{
+    public class Book:Author
+    {
+        private string isbn;
+        private string title;
+        private Author author;
+
+        public Book()
+        {
+
+        }
+        public Book(string isbn, string title, Author author)
+        {
+            this.isbn = isbn;
+            this.title = title;
+            this.author = author;
+        }
+        public string GetIsbn()
+        {
+            return isbn;
+        }
+        public void SetIsbn(string isbn)
+        {
+            this.isbn = isbn;
+        }
+        public string GetTitle()
+        {
+            return title;
+        }
+        public void SetTitle(string title)
+        {
+            this.title = title;
+        }
+        public Author GetAuthor()
+        {
+            return author;
+        }
+        public void SetAuthor(Author author)
+        {
+            this.author = author;
+        }
+        public override string ToString()
+        {
+            return "ISBN: " + isbn + " Title: " + title + " " + author.ToString();
+        }
+    }
+}
